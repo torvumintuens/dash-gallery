03c9d1f27fd80652b5c38a307b3cee54dd15e654
Sofya Lachugina <sofya.lachugina@yandex.ru>
2025-05-26 13:02:14 +0300

    изменение для запуска на сервере

--- main.py
+++ main.py
@@ -363,4 +363,5 @@ def upload_csv(contents):
     return dash.no_update, ""
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 8050))
+    app.run(host="0.0.0.0", port=port)
